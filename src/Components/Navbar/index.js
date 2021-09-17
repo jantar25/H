b621221ef3f8f45a02316a27c38619c3ebd32a61
unknown <glodybulambo@gmail.com>
2021-09-17 04:10:08 +0200

    NavBar done

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,12 +1,46 @@
 import React from 'react'
-import {Nav,NavContainer,NavLogo} from './Styles'
+import {FaBars} from 'react-icons/fa'
+import logo from '../../images/logoH.png';
+import {Nav,NavContainer,NavLogo,Logo,MobileIcon,NavMenu,NavItem,NavLinks,NavBtn,NavBtnLinks} from './Styles'
 
 const navBar = () => {
     return (
         <>
            <Nav>
              <NavContainer>
-                <NavLogo to='/'><image src='../../images/BG.jpg' alt='cake picture' /></NavLogo>
+                <NavLogo to='/'><Logo src={logo} alt="Logo"/></NavLogo>
+                <MobileIcon>
+                  <FaBars />
+                </MobileIcon>
+                <NavMenu>
+                  <NavItem>
+                    <NavLinks to='Accueil'>Accueil</NavLinks>
+                  </NavItem>
+                  <NavItem>
+                    <NavLinks to='À propos'>À propos</NavLinks>
+                  </NavItem>
+                  <NavItem>
+                    <NavLinks to='Programmes'>Programmes</NavLinks>
+                  </NavItem>
+                  <NavItem>
+                    <NavLinks to='Evénement'>Evénement</NavLinks>
+                  </NavItem>
+                  <NavItem>
+                    <NavLinks to='Festival'>Festival</NavLinks>
+                  </NavItem>
+                  <NavItem>
+                    <NavLinks to='Editions'>Editions</NavLinks>
+                  </NavItem>
+                  <NavItem>
+                    <NavLinks to='Contact'>Contact</NavLinks>
+                  </NavItem>
+                  <NavItem>
+                    <NavLinks to='Blog'>Blog</NavLinks>
+                  </NavItem>
+                  <NavBtn>
+                    <NavBtnLinks to='/SignIn'>S'abonner</NavBtnLinks>
+                  </NavBtn>
+                </NavMenu>
              </NavContainer>
            </Nav>
         </>
