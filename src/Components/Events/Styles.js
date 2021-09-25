3f6251038e2694060cad3fae29330915eb627593
unknown <glodybulambo@gmail.com>
2021-09-25 20:41:02 +0200

    Events over view

--- src/Components/Events/Styles.js
+++ src/Components/Events/Styles.js
@@ -2,10 +2,98 @@ import styled from 'styled-components'
 import aboutBg from '../../images/EventsBkg.png'
 
 export const EventsContent=styled.section`
-height:90vh;
+height:115vh;
 max-heigth:1100px;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
 position:relative;
 overflow:hidden;
 background-image: url(${aboutBg});
 background-size: cover;
+
+h1 {
+    font-size: 3rem;
+    background-color: #ee5166;
+    background-image: linear-gradient(315deg, #ee5166 0%, #f08efc 74%);
+    background-size: 100%;
+    -webkit-background-clip: text;
+    -moz-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    -moz-text-fill-color: transparent;
+  }
 `
+export const EventsTrail=styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+margin-top: 20px;
+`
+export const EventWrapper=styled.div`
+display: grid;
+grid-template-columns: 1fr 1fr 1fr;
+grid-template-rows: 1fr;
+position: relative;
+overflow: hidden;
+`
+
+export const EventCard=styled.div`
+margin: 10px 10px 30px;
+height: 570px;
+width: 400px;
+border: 5px;
+display: flex;
+flex-direction: column;
+justify-content: center;
+position: relative;
+background:blue;
+
+&:hover {
+    transform: scale(1.03);
+    margin: 0 10px;
+  }
+
+`
+
+export const EventImg=styled.img`
+width: 400px;
+height: 425px;
+object-fit: cover;
+transition: all 0.5s ease-out;
+
+`
+
+export const EventTitle=styled.h2`
+color: #000;
+padding: 0 10px;
+font-size: 40px;
+font-weight: bold;
+transition: all 1s ease-in-out;
+border-bottom: 5px solid #000; 
+`
+
+export const EventDescription=styled.p`
+padding: 10px;
+font-size: 18px;
+transition: all 1s ease-in-out;
+color: #000;
+`
+export const EventBtn=styled.button`
+width: 300px;
+padding: 10px;
+border: none;
+outline: none;
+text-decoration: none;
+margin-bottom: 10px;
+font-size: 20px;
+font-weight: bold;
+border-radius: 5px;
+background-color: #861657;
+background-image: linear-gradient(326deg, #861657 0%, #ffa69e 74%);
+transition: all 1s ease-in-out;
+cursor: pointer;
+`
+
+ 
--- src/Components/Events/index.js
+++ src/Components/Events/index.js
@@ -1,10 +1,36 @@
 import React from 'react'
-import {EventsContent} from './Styles'
+import {EventsContent,EventsTrail,EventWrapper,EventCard,EventImg,EventTitle,EventDescription,EventBtn} from './Styles'
+import Event1 from '../../images/Event1.JPG'
+import Event2 from '../../images/Event2.JPG'
+import Event3 from '../../images/Event3.JPG'
+
 
 const Events = () => {
     return (
         <EventsContent>
-          <h1>EVENTS</h1>  
+          <h1>EVENTS</h1> 
+          <EventsTrail>
+                    <EventWrapper>
+                        <EventCard>
+                            <EventImg src={Event1} alt={Event1} />
+                            <EventTitle>Meat Variety</EventTitle>
+                            <EventDescription>We labour the land whith natural means,no chemical product in our vegetable</EventDescription>
+                            <EventBtn>Purchase</EventBtn>
+                        </EventCard>
+                        <EventCard>
+                            <EventImg src={Event2} alt={Event2} />
+                            <EventTitle>Meat Variety</EventTitle>
+                            <EventDescription>We labour the land whith natural means,no chemical product in our vegetable</EventDescription>
+                            <EventBtn>Purchase</EventBtn>
+                        </EventCard>
+                        <EventCard>
+                            <EventImg src={Event3} alt={Event3} />
+                            <EventTitle>Meat Variety</EventTitle>
+                            <EventDescription>We labour the land whith natural means,no chemical product in our vegetable</EventDescription>
+                            <EventBtn>Purchase</EventBtn>
+                        </EventCard>
+                    </EventWrapper>
+                </EventsTrail> 
         </EventsContent>
     )
 }
