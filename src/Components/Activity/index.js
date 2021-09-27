a0ceecf3680b34b7446e65d5fa6d7814e6d9a24f
unknown <glodybulambo@gmail.com>
2021-09-27 18:06:41 +0200

    Activity done

--- src/Components/Activity/index.js
+++ src/Components/Activity/index.js
@@ -1,10 +1,48 @@
 import React from "react";
-import { ActivityContent } from "./Styles";
+import ActivityImage from "../../images/ActivityImage.jpg";
+import {
+  ActivityContent,
+  ActivityWrapper,
+  ActivityRows,
+  ActivityColumn1,
+  ImgWrap,
+  Img,
+  ActivityColumn2,
+  Text,
+  Presentation,
+  Heading,
+  Description,
+  Title,
+} from "./Styles";
 
 const Activity = () => {
   return (
     <ActivityContent>
-      <h1>Hello activity</h1>
+      <ActivityWrapper>
+        <ActivityRows>
+          <ActivityColumn1>
+            <ImgWrap>
+              <Img src={ActivityImage} alt="ActitivityImage" />
+            </ImgWrap>
+          </ActivityColumn1>
+          <ActivityColumn2>
+            <Text>
+              <Presentation>Nos Membres</Presentation>
+              <Heading>Ayant Une vie epanuis et Familiale</Heading>
+              <Description>
+                <Title>HORTELINS et HORTELINES</Title> ont une structure
+                Artistique et humanitaire dont la Mission est de Communiquer
+                pour pacifier en apportant a chacun et a tous la vraie paix et
+                le vrai amour du coeur. Ayant pour vision d'un present et un
+                avenir ou les relations des jeunes des differentes cultures sont
+                rendue plus humaines et plus proches. Oeuvrant dans
+                l'art,culture et Education;Paix et Engagement;Entrepreneuriat
+                social.
+              </Description>
+            </Text>
+          </ActivityColumn2>
+        </ActivityRows>
+      </ActivityWrapper>
     </ActivityContent>
   );
 };
