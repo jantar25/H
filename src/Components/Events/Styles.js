6577c10f1a36326f0fd3a3d0788141b08d552812
unknown <glodybulambo@gmail.com>
2021-10-02 03:43:28 +0200

    merge fixed

--- src/Components/Events/Styles.js
+++ src/Components/Events/Styles.js
@@ -38,7 +38,6 @@ export const EventWrapper = styled.div`
   overflow: hidden;
 `;
 
-<<<<<<< HEAD
 export const EventCard = styled.div`
   margin: 20px 10px 10px;
   height: 550px;
@@ -52,34 +51,15 @@ export const EventCard = styled.div`
   background: whitesmoke;
   box-shadow: 0 5px 5px rgba(0, 0, 0, 0.3), 0 0px 10px rgba(0, 0, 0, 0.3);
   transition: all 0.5s ease-in-out;
-=======
-export const EventCard=styled.div`
-margin: 10px 10px 30px;
-height: 550px;
-width: 400px;
-border: 5px;
-border-radius: 5px;
-display: flex;
-flex-direction: column;
-justify-content: center;
-position: relative;
-background:whitesmoke;
-box-shadow: 0 14px 28px rgba(0,0,0,0.7), 0 10px 10px rgba(0,0,0,0.22);
-transition: all 0.5s ease-out;
->>>>>>> d065706d0e03807f817b8b9b2669907cc0849f92
 
   &:hover {
     transform: scale(1.03);
     margin: 0 10px;
-<<<<<<< HEAD
     box-shadow: 0 10px 10px rgba(0, 0, 0, 0.7), 0 10px 10px rgba(0, 0, 0, 0.22);
-=======
     transition: all 0.5s ease-out;
->>>>>>> d065706d0e03807f817b8b9b2669907cc0849f92
   }
 `;
 
-<<<<<<< HEAD
 export const EventImg = styled.img`
   width: 400px;
   height: 425px;
@@ -94,48 +74,6 @@ export const EventTitle = styled.h2`
   width: 90%;
   border-bottom: 2px solid #a36a00;
 `;
-=======
-`
-
-export const EventImg=styled.img`
-width: 400px;
-height: 425px;
-object-fit: cover;
-
-`
-
-export const EventTitle=styled.h2`
-color: #003366;
-padding: 0 10px;
-font-size: 40px;
-font-weight: bold;
-transition: all 1s ease-in-out;
-border-bottom: 5px solid #a36a00; 
-`
-
-export const EventDescription=styled.p`
-padding: 10px;
-font-size: 18px;
-transition: all 1s ease-in-out;
-color: #000;
-`
-export const EventBtn=styled.button`
-width: 200px;
-padding: 10px;
-border: none;
-outline: none;
-text-decoration: none;
-margin-left: 10px;
-margin-bottom: 10px;
-font-size: 20px;
-font-weight: bold;
-border-radius: 5px;
-background-color: #861657;
-background-image: linear-gradient(326deg, #861657 0%, #ffa69e 74%);
-transition: all 1s ease-in-out;
-cursor: pointer;
-`
->>>>>>> d065706d0e03807f817b8b9b2669907cc0849f92
 
 export const EventDescription = styled.p`
   padding: 10px;
--- src/Components/Events/index.js
+++ src/Components/Events/index.js
@@ -14,7 +14,6 @@ import Event2 from "../../images/Event2.JPG";
 import Event3 from "../../images/Event3.JPG";
 
 const Events = () => {
-<<<<<<< HEAD
   return (
     <EventsContent>
       <h1>Nos Evénements</h1>
@@ -52,35 +51,5 @@ const Events = () => {
     </EventsContent>
   );
 };
-=======
-    return (
-        <EventsContent>
-          <h1>EVENTS</h1> 
-          <EventsTrail>
-                    <EventWrapper>
-                        <EventCard>
-                            <EventImg src={Event1} alt={Event1} />
-                            <EventTitle>WAUMBA Festival</EventTitle>
-                            <EventDescription>We labour the land whith natural means,no chemical product in our vegetable</EventDescription>
-                            <EventBtn>En Savoir Plus...</EventBtn>
-                        </EventCard>
-                        <EventCard>
-                            <EventImg src={Event2} alt={Event2} />
-                            <EventTitle>Tucheke Pamoja</EventTitle>
-                            <EventDescription>We labour the land whith natural means,no chemical product in our vegetable</EventDescription>
-                            <EventBtn>En Savoir Plus...</EventBtn>
-                        </EventCard>
-                        <EventCard>
-                            <EventImg src={Event3} alt={Event3} />
-                            <EventTitle>Sestri</EventTitle>
-                            <EventDescription>We labour the land whith natural means,no chemical product in our vegetable</EventDescription>
-                            <EventBtn>En Savoir Plus...</EventBtn>
-                        </EventCard>
-                    </EventWrapper>
-                </EventsTrail> 
-        </EventsContent>
-    )
-}
->>>>>>> d065706d0e03807f817b8b9b2669907cc0849f92
 
 export default Events;
